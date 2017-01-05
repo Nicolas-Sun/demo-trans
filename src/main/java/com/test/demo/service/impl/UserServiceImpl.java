f277dff6d18d6d6ebbfe7f75158a7c6c8eb6d25d
Sun Sentian <sunst20@chinaunicom.cn>
2017-01-05 16:52:31 +0800

    Revert "测试rollback"
    This reverts commit 3aa966fa919f78a3b1cfaf1e798e40370141b9c6.

--- src/main/java/com/test/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/test/demo/service/impl/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.test.demo.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +10,11 @@ import com.test.demo.service.IUserService;
 @Service
 public class UserServiceImpl implements IUserService{
 	
-	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
-	
 	@Autowired
 	private IUserDao userDao;
 
 	public UserServiceImpl() {
-		logger.debug("test rollback!");
+		
 	}
 
 	public User getUserById(int id) {
